7388ed63551110b4c91752c0ceedd7d49f863c1d
stsoy01 <tsoy@perco.ru>
2024-06-14 15:04:52 +0300

    upd

--- server/scripts/server.js
+++ server/scripts/server.js
@@ -1,32 +1,29 @@
+const path = require("path");
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')})
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser')
-const fs = require('fs')
+const cors = require('cors');
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
 
 app.use(cors({
-  origin: 'http//:localhost:1000',
-  optionsSuccessStatus: 200,
-  methods: 'GET,PUT,POST'
+    origin: 'http//:localhost:1000',
+    optionsSuccessStatus: 200,
+    methods: 'GET,PUT,POST'
 }))
 
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.post('/auth', (req, res) => {
-  let userData;
-
-  fs.readFile('../data/user.json', 'utf8', (err, data) => {
-    console.log(data);
-  })
-
-
-  res.json(req.body)
+    const accessToken = jwt.sign(req.body, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+    res.json({accessToken: accessToken})
 })
 
 
 app.listen(2000, () => {
-  console.log('server is working')
+    console.log('server is working')
 })
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,41 @@
 import '../styles/style.css';
-import { verifyUser } from './auth'
+import {verifyUser} from './auth';
 
 
+const email_btn = document.getElementsByClassName('email_btn')[0];
+const password_btn = document.getElementsByClassName('password_btn')[0];
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-const button = document.getElementById('btn')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const authData = {}
+const auth_email = document.getElementsByClassName('auth_email')[0];
+const auth_password = document.getElementsByClassName('auth_password')[0];
+const authData = {};
 
-button.addEventListener('click', (e) => {
-  authData.email = email.value;
-  authData.password = password.value;
+password.style.display = 'none';
+auth_password.style.display = 'none';
+password_btn.style.display = 'none';
 
-  if (Object.values(authData).includes('')) {
-    alert('fill all inputs')
-    return;
-  }
-  
-  verifyUser('http://localhost:2000/auth', authData)
-    .then(res => res.text())
-    .then(res => console.log(res))
+
+email_btn.addEventListener('click', (e) => {
+    authData.email = email.value;
+
+    if (!Object.values(authData)[0]) {
+        alert('EMail field can`t be empty');
+        return;
+    }
+
+    password.style.display = 'block';
+    auth_password.style.display = 'block';
+    password_btn.style.display = 'block';
+
+    auth_email.style.display = 'none';
+    email.style.display = 'none';
+    email_btn.style.display = 'none';
+})
+
+password_btn.addEventListener('click', () => {
+    authData.password = password.value;
+    verifyUser('http://localhost:2000/auth', authData)
+        .then(res => res.text())
+        .then(res => console.log(res));
 })
